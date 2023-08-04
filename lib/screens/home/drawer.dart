954c17866a4a5bb5748170ef6a1cd660eb4f61f2
karthik <karthikprabhu23223@gmail.com>
2023-08-04 22:42:56 +0530

    Added onTap redirection on drawer.dart

--- lib/screens/home/drawer.dart
+++ lib/screens/home/drawer.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:shop_flutter/constants/colorfile.dart';
+import 'package:shop_flutter/screens/home/homescreen.dart';
 import 'package:shop_flutter/screens/profile/my_profile.dart';
 
 class DrawerList extends StatelessWidget {
@@ -31,74 +32,110 @@ class DrawerList extends StatelessWidget {
         child: ListView(
           children: [
             Column(
-              children:[
-              SizedBox(
-                height: 230,
-                child: DrawerHeader(
-                  child: Column(children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.black45,
-                      radius: 48,
-                      child: CircleAvatar(
-                        backgroundColor: Colors.white24,
-                        backgroundImage: AssetImage("assets/b2.jpeg"),
-                        radius: 45,
+              children: [
+                SizedBox(
+                  height: 230,
+                  child: DrawerHeader(
+                    child: Column(children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.black45,
+                        radius: 48,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.white24,
+                          backgroundImage: AssetImage("assets/b2.jpeg"),
+                          radius: 45,
+                        ),
                       ),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          height: 17,
-                        ),
-                        Text(
-                          'Welcome User,',
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(3.0),
-                          child: Container(
-                            height: 24,
-                            child: OutlinedButton(
-                              onPressed: () {},
-                              child: Text(
-                                'Login',
-                                style: TextStyle(color: Colors.white),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 17,
+                          ),
+                          Text(
+                            'Welcome User,',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(3.0),
+                            child: Container(
+                              height: 24,
+                              child: OutlinedButton(
+                                onPressed: () {},
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(color: Colors.white),
+                                ),
                               ),
                             ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ]),
+                          )
+                        ],
+                      ),
+                    ]),
+                  ),
                 ),
-              ),
-              Column(
-                children: [
-                  listTile(
-                    icon: Icons.home,
-                    title: "Home",
-                  ),
-                  listTile(
-                    icon: Icons.favorite,
-                    title: "Wishlist",
-                  ),
-                  listTile(
-                    icon: Icons.shopping_cart_rounded,
-                    title: "Cart",
-                  ),
-                  listTile(
-                    icon: Icons.notifications_active,
-                    title: "Notifications",
-                  ),
-                  listTile(
-                    icon: Icons.format_quote_rounded,
-                    title: "More info",
-                  ),
-                  listTile(
+                Column(
+                  children: [
+                    listTile(
+                      icon: Icons.home,
+                      title: "Home",
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => HomeScreen(),
+                          ),
+                        );
+                      },
+                    ),
+                    listTile(
+                      icon: Icons.favorite,
+                      title: "Wishlist",
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => MyProfile(),
+                          ),
+                        );
+                      },
+                    ),
+                    listTile(
+                      icon: Icons.shopping_cart_rounded,
+                      title: "Cart",
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => MyProfile(),
+                          ),
+                        );
+                      },
+                    ),
+                    listTile(
+                      icon: Icons.notifications_active,
+                      title: "Notifications",
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => MyProfile(),
+                          ),
+                        );
+                      },
+                    ),
+                    listTile(
+                      icon: Icons.format_quote_rounded,
+                      title: "More info",
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => MyProfile(),
+                          ),
+                        );
+                      },
+                    ),
+                    listTile(
                       icon: Icons.person,
                       title: "My Profile",
                       onTap: () {
@@ -107,17 +144,18 @@ class DrawerList extends StatelessWidget {
                             builder: (context) => MyProfile(),
                           ),
                         );
-                      }),
-                  listTile(
-                    icon: Icons.home,
-                    title: "Home",
-                  ),
-                  listTile(
-                    icon: Icons.home,
-                    title: "Home",
-                  ),
-                ],
-              ),
+                      },
+                    ),
+                    listTile(
+                      icon: Icons.home,
+                      title: "Home",
+                    ),
+                    listTile(
+                      icon: Icons.home,
+                      title: "Home",
+                    ),
+                  ],
+                ),
               ],
             ),
           ],
