788ecc6ae42ea54679cddc52389652f8e7a6de7b
karthik <karthikprabhu23223@gmail.com>
2023-08-07 22:19:33 +0530

    FireCloud

---
+++ lib/model/product_model.dart
@@ -0,0 +1,8 @@
+class ProductModel {
+  String proName;
+  String proImg;
+  int proPrice;
+
+  ProductModel(
+      {required this.proName, required this.proImg, required this.proPrice});
+}
---
+++ lib/providers/product_provider.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:shop_flutter/model/product_model.dart';
+
+class ProductProvider with ChangeNotifier {
+  List<ProductModel> ProductList = [];
+
+  fetchProductData() {
+
+    FirebaseFirestore.instance.collection("").get();
+  }
+}
